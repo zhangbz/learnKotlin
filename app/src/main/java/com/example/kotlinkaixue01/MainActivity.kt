b3325574fe1f71a05f52bfec1fe0f60b28df5da6
zhangbaozhi <janiszhang@tencent.com>
2019-08-21 15:22:01 +0800

    码上开学 kotlin 同步练习 2 Kotlin 里那些「不是那么写的」

--- app/src/main/java/com/example/kotlinkaixue01/MainActivity.kt
+++ app/src/main/java/com/example/kotlinkaixue01/MainActivity.kt
@@ -14,9 +14,65 @@ class MainActivity : AppCompatActivity() {
         view = findViewById(R.id.tv)
 
         printViewId(view)
+
+        println(Singleton.id.toString() + " : " + Singleton.getName())
+
+        test02()
     }
 
     fun printViewId(view: View?): Unit {
         println("view id = " + view?.id)
+    }
+
+    fun test02(){
+
+        var array= arrayOfNulls<Int>(100000)
+//        var intArr = intArrayOf()
+        var intArr = IntArray(100000)
+        var list = mutableListOf<Int>()
+
+        var sum =0
+        var average = 0
+
+        var beginTime = System.currentTimeMillis()
+        for (i in 1..100000) {
+            array[i-1] = i
+            sum+=i
+        }
+
+        average = sum/100000
+
+        println("array 平均值：" + average + ", 操作耗时：" + (System.currentTimeMillis() - beginTime))
+
+        sum = 0
+        beginTime = System.currentTimeMillis()
+
+        for (i in 1..100000) {
+            intArr[i-1] = i
+            sum+=i
+        }
+
+        average = sum/100000
+
+        println("intArr 平均值：" + average + ", 操作耗时：" + (System.currentTimeMillis() - beginTime))
+
+
+        sum = 0
+        beginTime = System.currentTimeMillis()
+
+        for (i in 1..100000) {
+            list.add(i)
+            sum+=i
+        }
+
+        average = sum/100000
+
+        println("list 平均值：" + average + ", 操作耗时：" + (System.currentTimeMillis() - beginTime))
+
+
+//        array 平均值：7050, 操作耗时：119
+//        intArr 平均值：7050, 操作耗时：5   //效率惊人
+//        list 平均值：7050, 操作耗时：292
+
     }
 }
---
+++ app/src/main/java/com/example/kotlinkaixue01/Singleton.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinkaixue01
+
+object Singleton {
+    var id = 1
+
+    fun getName(): String {
+        return "Bob"
+    }
+}
